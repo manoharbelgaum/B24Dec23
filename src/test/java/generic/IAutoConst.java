a6b6aba1be9011f44cba3ed8e61d54c7391aa3ac
My Account <My Account@LAPTOP-KE5HNIIP>
2021-12-23 08:38:52 +0530

    Draft copy

---
+++ src/test/java/generic/IAutoConst.java
@@ -0,0 +1,8 @@
+package generic;
+
+public interface IAutoConst {
+	
+	String XL_PATH="./src/test/resources/data/input.xlsx";
+	String APP_URL="http://www.google.com";
+	String PPT_PATH="./src/test/resources/config.properties";
+}
